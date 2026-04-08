a973288ef71c6306dc0f54d898b0d80b2d3fde13
banibu <guicjs14@gmail.com>
2026-04-08 15:19:06 -0300

    primeiro commit

---
+++ src/main/java/org/crystalocean/listEvent/ListEvent.java
@@ -0,0 +1,16 @@
+package org.crystalocean.listEvent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ListEvent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
